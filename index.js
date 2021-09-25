bed9525a85c55af57733ce54f49a115631da9678
Gurmanshaan Singh <gurmanshaan@gmail.com>
2021-09-25 18:42:10 +0530

    made cash register manager
    create html and javascript file
    add styles for laptop version

---
+++ index.js
@@ -0,0 +1,48 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+      const amountToBeReturned = cashGiven.value - billAmount.value;
+      calculatechange(amountToBeReturned);
+    } else {
+      showMessage(
+        "The cash provided should atleast be equal to the bill amount."
+      );
+    }
+  } else {
+    showMessage("Invalid Bill Amount.");
+  }
+});
+
+function calculatechange(amountToBeReturned) {
+  // go over all the availabe notes
+
+  for (let i = 0; i < availableNotes.length; i++) {
+    //no of notes need for the denomination
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+
+    //amount left after the no of notes needed
+
+    //amountToBeReturned %= availableNotes[i];
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+
+    // updating the no of notes in the table for current amount
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
